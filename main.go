43a53067db0bf36139ab4cd1e1d34388d2692cc9
skywhat <i@skywhat.com>
2025-03-02 21:04:31 -0800

    b tree node structure

--- main.go
+++ main.go
@@ -44,6 +44,41 @@ func SaveData2(path string, data []byte) error {
 	return os.Rename(tmp, path) // replace the original file
 }
 
+type BNode struct {
+	data []byte
+}
+
+const (
+	BNODE_NODE = 1 // internal node without values
+	BNODE_LEAF = 2 // leaf node with values
+)
+
+type BTree struct {
+	root uint64
+	// callbacks for managing on disk pages
+	get func(uint64) BNode // dereference a pointer
+	new func(BNode) uint64 // allocate a new page
+	del func(uint64)       // deallocate a page
+}
+
+const HEADER = 4
+
+const BTREE_PAGE_SIZE = 4096
+const BTREE_MAX_KEY_SIZE = 1000
+const BTREE_MAX_VAL_SIZE = 3000
+
+func assert(cond bool) {
+	if !cond {
+		panic("assertion failed")
+	}
+}
+
+func init() {
+	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+	assert(node1max <= BTREE_PAGE_SIZE)
+}
+
 func main() {
 	SaveData2("test.txt", []byte("hello"))
+
 }
